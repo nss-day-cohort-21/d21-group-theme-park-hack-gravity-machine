9e9432340515320d9d6ddbc436a18aa6016e6a60
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-08-16 13:43:14 -0500

    added nameString.js, added require in main

--- javascripts/loader.js
+++ javascripts/loader.js
@@ -6,30 +6,31 @@ let areaArray = [];
 let areaObject = {};
 
 
-function fillAreaArray (data) {
+function fillAreaArray(data) {
     let keys = Object.keys(data);
-    keys.forEach((item)=> {
-      data[item].firebaseID = item;
-      areaArray.push(data[item]);
-  });
+    keys.forEach((item) => {
+        data[item].firebaseID = item;
+        areaArray.push(data[item]);
+    });
 }
 
 areaObject.getAreaArray = () => {
-	return areaArray;
+    return areaArray;
 };
 
 
-areaObject.loadAreaArray =  () => {
-  return new Promise( function (resolve, reject) {
-    var areaLoader = new XMLHttpRequest();
-    areaLoader.open("GET", "https://theme-park-cfa10.firebaseio.com/areas.json");
-    areaLoader.send();
-    areaLoader.addEventListener("load", function() {
-      var data = JSON.parse(this.responseText);
-      resolve(data); // No longer responsible for calling populatePage function
+areaObject.loadAreaArray = () => {
+    return new Promise(function(resolve, reject) {
+        var areaLoader = new XMLHttpRequest();
+        areaLoader.open("GET", "https://theme-park-cfa10.firebaseio.com/areas.json");
+        areaLoader.send();
+        areaLoader.addEventListener("load", function() {
+            var data = JSON.parse(this.responseText);
+            resolve(data); // No longer responsible for calling populatePage function
+        });
     });
-  });
 };
+
+
 
 module.exports = areaObject;
-
--- javascripts/search.js
+++ javascripts/search.js
@@ -1,11 +1,21 @@
 "use strict";
 
+console.log("search.js");
+
+
+// let Handlebars = require('hbsfy/runtime'),
+//     areaAquire = require('./loader.js'),
+//     attracAquire = require('./attracLoader.js');
+
+//Get button and input area from DOM
 let submitBtn = document.getElementById("basic-addon1");
 let inputArea = document.getElementById("input");
 
-
+//Button event listener
 submitBtn.addEventListener("click", submit);
 
+//Makes button pull input value
+// This Works
 function submit() {
     event.preventDefault();
     var userInput = inputArea.value;
@@ -13,16 +23,28 @@ function submit() {
 
 }
 
+//makes enter key pull input value
+//This works
 $("input").on("keydown", function search(e) {
     if (e.keyCode == 13) {
+        //stops form submit button from refreshing page by default
         event.preventDefault();
-        console.log($(this).val());
+        console.log("user input:", $(this).val());
     }
 });
 
 
+//WIP-not sure where this is at
+$("#data-panel").append(inputSearch());
+
 function populateSearchResults(stuff) {
     let newDiv = document.createElement("div");
-    newDiv.innerHTML = searchAquire(stuff);
+    newDiv.innerHTML = inputSearch(stuff);
     $("#search-results").append(newDiv);
+    console.log();
+}
+
+
+function inputSearch() {
+
 }
